57a7b8528b20394d76e10f435eb616f3bf0b5943
Tom Dunlap <prof_myu@yahoo.com>
2017-05-18 18:28:18 -0400

    added bootstrap pieces, map function, and marker functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,87 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  //Zoom level
+  zoom: number = 10;
+  //Start Position City Council
+  lat: number = 40.0153;
+  lng: number = -105.2798;
+  //Values
+  markerName:string;
+  markerLat:string;
+  markerLng:string;
+  markerDraggable:string;
+  //Markers
+  markers: marker[] = [
+    {
+      name: 'Marker 1',
+      lat: 40.0153,
+      lng: -105.2701,
+      draggable: true
+    },
+    {
+      name: 'Marker 2',
+      lat: 40.0453,
+      lng: -105.2788,
+      draggable: true
+    }
+  ];
+
+  constructor() {
+
+  }
+
+  clickedMarker(marker:marker, index:number){
+    console.log('Clicked Marker: '+marker.name+' at index '+index);
+  }
+
+  mapClicked($event:any){
+    var newMarker = {
+      name: 'Untitled',
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: false
+    }
+
+    this.markers.push(newMarker);
+  }
+
+  markerDragEnd(marker:any, $event:any){
+    console.log('dragEnd', marker, $event);
+
+    var updMarker = {
+      namer: marker.name,
+      lat: parseFloat(marker.lat),
+      lng: parseFloat(marker.lng),
+      draggable: false
+    }
+
+    var newLat = $event.coords.lat;
+    var newLng = $event.coords.lng;
+  }
+
+  addMarker(){
+    console.log("Adding Marker");
+    if(this.markerDraggable == 'yes'){
+      var isDraggable = true;
+    } else {
+      var isDraggable = false;
+    }
+
+    var newMarker = {
+      name: this.markerName,
+      lat: parseFloat(this.markerLat),
+      lng: parseFloat(this.markerLng),
+      draggable: isDraggable
+    }
+
+    this.markers.push(newMarker);
+  }
+}
+
+interface marker{
+  name?: string;
+  lat: number;
+  lng: number;
+  draggable: boolean;
 }
